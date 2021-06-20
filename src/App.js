640c0fcd7034f537c78935f68608a76d96173e47
alanrich-dev <alan.a.rich@gmail.com>
2021-06-20 18:49:05 +0200

    added logic to submit edits

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ function App() {
 /*************************** State ****************************/
 const [todos, setTodos] = useState([]);
 const [todo, setTodo] = useState('');
+const [editingText, setEditingText] = useState('');
+// When edit task btn is clicked, the id of that task is sent to todoEditing
+// telling the app which task to convert from text to an editable input
+const [todoEditing, setTodoEditing] = useState(null);
 
 /*************************** Methods ****************************/
 
 // add the user input to state and reset the input
 function handleSubmit(e){
   e.preventDefault();
-// build an object out of the user input
   const newTodo = {
     id: new Date().getTime(),
     text: todo,
@@ -29,7 +32,7 @@ function deleteTodo(someId) {
   setTodos(updatedTodos);
 }
 
-// so why did we have to iterate the array rather than just directly toggle the item inline
+// why did we have to iterate the array rather than just directly toggle the item inline
 function toggleCompleted(someId) {
   const updatedTodos = [...todos].map((todo) => {if (todo.id === someId) {
       todo.completed = !todo.completed;
@@ -38,14 +41,30 @@ function toggleCompleted(someId) {
   });
   setTodos(updatedTodos);
 }
+// assign the edited value to the task with matching id, reset all state vars.
+// except the one the method modifies
+function editTodo(someId){
+  const updatedTodos = [...todos].map((todo) => {
+    if(todo.id === someId){
+       todo.text = editingText;
+    };
+    return todo;
+  });
+  setTodos(updatedTodos);
+  setTodoEditing(null);
+  setEditingText("");
+}
 /**************************** JSX ****************************/
   return (
     <div className="App">
       <form action="" onSubmit={handleSubmit}>
+      {/*the inputs employ identical logic by assigning the value of the event target
+        property to a state variable when the user types and then using 2-way data binding
+        by assigning the newly created state to the input's own value*/}
         <input type="text"
                placeholder='enter a task'
                onChange={(e) => setTodo(e.target.value)}
-              /* utilize 2 way data binding */
+
               value={todo}
         />
         <button>add task</button>
@@ -53,13 +72,32 @@ function toggleCompleted(someId) {
       <ul>
 
         {todos.map((todo) =>
+
           <li key={todo.id}>
-            {todo.text}
+            {/*render the task as text or an input if the user clicks edit btn*/}
+            {todo.id === todoEditing ?
+
+              (<div>
+                 <input type="text"
+                    onChange={(e) => setEditingText(e.target.value)}
+                    value={editingText}/>
+                 <button onClick={() => editTodo(todo.id)}>Submit</button>
+
+              </div>)
+                                    :
+              (<div>{todo.text}</div>)
+            }
+
+
+            <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+
             <button onClick={() => deleteTodo(todo.id)}> Delete </button>
             <input type='checkbox'
                    onChange={() => toggleCompleted(todo.id)}
-            />
+                   checked={todo.completed}/>
+
           </li>
+
         )}
 
       </ul>
